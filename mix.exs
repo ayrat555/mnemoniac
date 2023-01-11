a72f36776b39ddcd25e4e6274898f0156ecf95cf
Ayrat Badykov <ayratin555@gmail.com>
2023-01-11 18:55:12 +0200

    add github actions

--- mix.exs
+++ mix.exs
@@ -21,9 +21,10 @@ defmodule Mnemoniac.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:cryptopunk, "~> 0.6.6", only: :test}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:cryptopunk, "~> 0.6.6", only: :test},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
