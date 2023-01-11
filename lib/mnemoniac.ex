f6255e56850d2161654c4963d7ce654ff7ad8563
Ayrat Badykov <ayratin555@gmail.com>
2023-01-11 19:43:48 +0200

    add more functions

--- lib/mnemoniac.ex
+++ lib/mnemoniac.ex
@@ -4,8 +4,9 @@ defmodule Mnemoniac do
 
   See https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
   """
-  @word_number_to_entropy_bits %{12 => 128, 15 => 160, 18 => 192, 21 => 224, 24 => 256}
-  @word_numbers Map.keys(@word_number_to_entropy_bits)
+  @word_numbers_to_entropy_bits %{12 => 128, 15 => 160, 18 => 192, 21 => 224, 24 => 256}
+  @word_numbers Map.keys(@word_numbers_to_entropy_bits)
+  @entropy_bits_sizes Map.values(@word_numbers_to_entropy_bits)
   @words :mnemoniac
          |> :code.priv_dir()
          |> Path.join("words")
@@ -13,39 +14,78 @@ defmodule Mnemoniac do
          |> Stream.map(&String.trim/1)
          |> Enum.to_list()
 
-  @spec create_mnemonic(non_neg_integer()) :: String.t() | no_return
+  @spec create_mnemonic(non_neg_integer()) :: {:ok, String.t()} | {:error, :invalid_number}
   def create_mnemonic(word_number \\ Enum.max(@word_numbers))
 
   def create_mnemonic(word_number) when word_number not in @word_numbers do
-    raise ArgumentError,
-      message:
-        "Number of words #{inspect(word_number)} is not supported, please use one of the #{inspect(@word_numbers)}"
+    {:error, :invalid_number}
   end
 
   def create_mnemonic(word_number) do
-    entropy_bits = Map.fetch!(@word_number_to_entropy_bits, word_number)
+    entropy_bits = Map.fetch!(@word_numbers_to_entropy_bits, word_number)
 
-    entropy_bits
-    |> create_entropy()
-    |> do_create_from_entropy(entropy_bits)
+    mnemonic =
+      entropy_bits
+      |> create_entropy()
+      |> do_create_from_entropy(entropy_bits)
+
+    {:ok, mnemonic}
   end
 
-  @spec create_mnemonic_from_entropy(binary()) :: String.t() | no_return
+  @spec create_mnemonic!(non_neg_integer()) :: String.t() | no_return()
+  def create_mnemonic!(word_number \\ Enum.max(@word_numbers)) do
+    case create_mnemonic(word_number) do
+      {:ok, mnemonic} ->
+        mnemonic
+
+      _ ->
+        raise ArgumentError,
+          message:
+            "Number of words #{inspect(word_number)} is not supported, please use one of the #{inspect(@word_numbers)}"
+    end
+  end
+
+  @spec create_mnemonic_from_entropy(binary()) :: {:ok, String.t()} | {:error, :invalid_entropy}
   def create_mnemonic_from_entropy(entropy) do
     found_entropy_bits =
-      Enum.find(@word_number_to_entropy_bits, fn {_number, bits} ->
+      Enum.find(@word_numbers_to_entropy_bits, fn {_number, bits} ->
         div(bits, 8) == byte_size(entropy)
       end)
 
     case found_entropy_bits do
       {_, entropy_bits} ->
-        do_create_from_entropy(entropy, entropy_bits)
+        mnemonic = do_create_from_entropy(entropy, entropy_bits)
+
+        {:ok, mnemonic}
+
+      _ ->
+        {:error, :invalid_entropy}
+    end
+  end
+
+  @spec create_mnemonic_from_entropy!(binary()) :: String.t() | no_return
+  def create_mnemonic_from_entropy!(entropy) do
+    case create_mnemonic_from_entropy(entropy) do
+      {:ok, mnemonic} ->
+        mnemonic
 
       _ ->
         raise ArgumentError,
           message: "Entropy size is invalid"
     end
   end
+
+  @spec words() :: [String.t()]
+  def words, do: @words
+
+  @spec word_numbers_to_entropy_bits() :: %{non_neg_integer() => non_neg_integer()}
+  def word_numbers_to_entropy_bits, do: @word_numbers_to_entropy_bits
+
+  @spec word_numbers() :: [non_neg_integer()]
+  def word_numbers, do: @word_numbers
+
+  @spec entropy_bit_sizes() :: [non_neg_integer()]
+  def entropy_bit_sizes, do: @entropy_bits_sizes
 
   defp do_create_from_entropy(entropy, entropy_bits) do
     entropy
--- test/mnemoniac_test.exs
+++ test/mnemoniac_test.exs
@@ -4,16 +4,30 @@ defmodule MnemoniacTest do
   alias Cryptopunk.Key
   alias Cryptopunk.Seed
 
-  describe "create/1" do
-    test "creates mnemonic" do
-      words = Mnemoniac.create_mnemonic()
+  describe "create_mnemonic/1" do
+    test "creates mnemonics of different sizes" do
+      Enum.each([12, 15, 18, 21, 24], fn number_of_words ->
+        assert {:ok, mnemonic} = Mnemoniac.create_mnemonic(number_of_words)
 
-      assert 24 == words |> String.split() |> Enum.count()
+        assert number_of_words == count_words(mnemonic)
+      end)
+    end
+
+    test "fails to generate a mnemonic if the number of words is invalid" do
+      assert {:error, :invalid_number} = Mnemoniac.create_mnemonic(11)
+    end
+  end
+
+  describe "create_mnemonic!/1" do
+    test "creates mnemonic with 24 words by default" do
+      mnemonic = Mnemoniac.create_mnemonic!()
+
+      assert 24 == count_words(mnemonic)
     end
 
     test "generates different mnemonics" do
-      words1 = Mnemoniac.create_mnemonic()
-      words2 = Mnemoniac.create_mnemonic()
+      words1 = Mnemoniac.create_mnemonic!()
+      words2 = Mnemoniac.create_mnemonic!()
 
       assert words1 != words2
     end
@@ -22,7 +36,7 @@ defmodule MnemoniacTest do
       assert_raise ArgumentError,
                    "Number of words 10 is not supported, please use one of the [12, 15, 18, 21, 24]",
                    fn ->
-                     Mnemoniac.create_mnemonic(10)
+                     Mnemoniac.create_mnemonic!(10)
                    end
     end
 
@@ -30,21 +44,44 @@ defmodule MnemoniacTest do
       Enum.each([12, 15, 18, 21, 24], fn number_of_words ->
         assert number_of_words ==
                  number_of_words
-                 |> Mnemoniac.create_mnemonic()
-                 |> String.split(" ")
-                 |> Enum.count()
+                 |> Mnemoniac.create_mnemonic!()
+                 |> count_words()
       end)
+    end
+  end
+
+  describe "create_mnemonic_from_entropy!/1" do
+    test "create mnemonic from the provided entropy" do
+      bytes =
+        <<6, 197, 169, 93, 98, 210, 82, 216, 148, 177, 1, 251, 142, 15, 154, 85, 140, 0, 13, 202,
+          234, 160, 129, 218>>
+
+      result = Mnemoniac.create_mnemonic_from_entropy!(bytes)
+
+      expected_result =
+        "almost coil firm shield cement hobby fan cage wine idea track prison scale alone close favorite limb still"
+
+      assert expected_result == result
+    end
+
+    test "fails if entropy is invalid" do
+      assert_raise ArgumentError,
+                   "Entropy size is invalid",
+                   fn ->
+                     Mnemoniac.create_mnemonic_from_entropy!(<<1>>)
+                   end
     end
 
     # https://github.com/trezor/python-mnemonic/blob/master/vectors.json
     test "verifies with bip tests" do
       %{"english" => tests} =
-        File.read!("test/support/mnemonic_test.json")
+        "test/support/mnemonic_test.json"
+        |> File.read!()
         |> Jason.decode!()
 
       for [entropy, mnemonic, expected_seed, extended_private_key] <- tests do
         {:ok, entropy} = Base.decode16(entropy, case: :lower)
-        assert mnemonic == Mnemoniac.create_mnemonic_from_entropy(entropy)
+        assert mnemonic == Mnemoniac.create_mnemonic_from_entropy!(entropy)
 
         seed = Seed.create(mnemonic, "TREZOR")
         assert expected_seed == Base.encode16(seed, case: :lower)
@@ -55,18 +92,52 @@ defmodule MnemoniacTest do
     end
   end
 
-  describe "create_from_entropy/2" do
-    test "create mnemonic from the provided entropy" do
-      bytes =
-        <<6, 197, 169, 93, 98, 210, 82, 216, 148, 177, 1, 251, 142, 15, 154, 85, 140, 0, 13, 202,
-          234, 160, 129, 218>>
+  describe "create_mnemonic_from_entropy" do
+    test "generates mnemonic from entropies of different sizes" do
+      Mnemoniac.word_numbers_to_entropy_bits()
+      |> Enum.each(fn {number_of_words, entropy_size} ->
+        byte_size = div(entropy_size, 8)
+        entropy = :crypto.strong_rand_bytes(byte_size)
 
-      result = Mnemoniac.create_mnemonic_from_entropy(bytes)
+        assert {:ok, mnemonic} = Mnemoniac.create_mnemonic_from_entropy(entropy)
 
-      expected_result =
-        "almost coil firm shield cement hobby fan cage wine idea track prison scale alone close favorite limb still"
-
-      assert expected_result == result
+        assert number_of_words == count_words(mnemonic)
+      end)
     end
+
+    test "fails if entropy bytes is invalid" do
+      assert {:error, :invalid_entropy} = Mnemoniac.create_mnemonic_from_entropy(<<1>>)
+    end
+  end
+
+  describe "words/0" do
+    test "returns all words" do
+      assert 2048 == Mnemoniac.words() |> Enum.count()
+    end
+  end
+
+  describe "word_numbers_to_entropy_bits/0" do
+    test "returns a map of word numbers to entropy bits" do
+      assert %{12 => 128, 15 => 160, 18 => 192, 21 => 224, 24 => 256} ==
+               Mnemoniac.word_numbers_to_entropy_bits()
+    end
+  end
+
+  describe "word_numbers/0" do
+    test "returns a list of supported word numbers in mnemonic" do
+      assert [12, 15, 18, 21, 24] == Mnemoniac.word_numbers()
+    end
+  end
+
+  describe "entopy_bit_sizes/0" do
+    test "returns supported entopy bit sizes" do
+      assert [128, 160, 192, 224, 256] == Mnemoniac.entropy_bit_sizes()
+    end
+  end
+
+  defp count_words(mnemonic) do
+    mnemonic
+    |> String.split(" ")
+    |> Enum.count()
   end
 end
